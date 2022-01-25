0e751ed2d68426b0d4b2ec20fc03eb17f12ce84d
patrick_cuentas <patrickcuentasmariano@gmail.com>
2022-01-24 20:49:44 -0500

    Update TodoApp.jsx

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,10 +1,44 @@
-import { useState } from "react";
+import { TaskForm } from "./components/TaskForm";
+import { TaskItemContainer } from "./components/TaskItemContainer";
+import { Footer } from "./ui/Footer";
 import { Header } from "./ui/Header";
+import { MoonIcon } from "./svgs/MoonIcon";
+import { SunIcon } from "./svgs/SunIcon";
+import { useSelector, useDispatch } from "react-redux";
+import { toogleThemeMode } from "./features/themeSlice";
+import { handleTheme } from "./utils/handleTheme";
+import { useEffect } from "react";
+
 export const TodoApp = () => {
+  const dispatch = useDispatch();
+  const themeMode = useSelector((state) => state.theme.themeMode);
+  const tasks = useSelector((state) => state.task.tasks);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
   return (
-    <div className="">
-      <Header theme={"dark"} />
-     
+    <div className="relative h-screen bg-light-theme-bg dark:bg-dark-theme-bg">
+      <Header theme={themeMode} />
+      <main className="max-w-xl mx-auto py-10 px-5 relative">
+        <div className="flex flex-wrap justify-between items-center">
+          <h1 className="text-[#fff] text-2xl md:text-3xl lg:text-4xl font-bold tracking-[.3em]">
+            TODO
+          </h1>
+          <button
+            onClick={() => handleTheme(themeMode, dispatch, toogleThemeMode)}
+          >
+            {themeMode === "DARK" ? <SunIcon /> : <MoonIcon />}
+          </button>
+        </div>
+
+        <TaskForm />
+
+        <TaskItemContainer />
+
+        <Footer />
+      </main>
     </div>
   );
 };
