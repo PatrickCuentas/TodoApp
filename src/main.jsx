f2a86597b0ee13ee07c922c73580ebdc2c7be8fb
patrick_cuentas <patrickcuentasmariano@gmail.com>
2022-01-29 18:40:38 -0500

    Add package react-grid-layout in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { render } from "react-dom";
 import { TodoApp } from "./TodoApp";
 import { store } from "./store/store";
 import { Provider } from "react-redux";
+import {GridLayoutComponent} from "./components/GridLayoutComponent.jsx"
 import "./index.css";
 
 const rootEl = document.getElementById("root");
